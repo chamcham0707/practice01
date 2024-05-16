0b3a845e7b6e32218d64d64a9d1e3a7e41c2c406
Suncheol Kim <tinymin@yahoo.co.kr>
2024-05-16 21:52:33 +0900

    Spring Boot 최초 등록

---
+++ src/main/java/kr/sparta/ttougayeon/TtouGayeonApplication.java
@@ -0,0 +1,13 @@
+package kr.sparta.ttougayeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtouGayeonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtouGayeonApplication.class, args);
+    }
+
+}
